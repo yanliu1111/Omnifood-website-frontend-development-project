755cc74c111554171db433df7b163b8b5f0f49cf
yanliu1111 <liuyan0504@gmail.com>
2024-03-17 23:32:07 -0600

    feat: smooth scrolling for all browsers using JS

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open'); // adding and removing at the same time
 });
+
+// smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+// console.log(allLinks);
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    if (href === '#') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  });
+});
